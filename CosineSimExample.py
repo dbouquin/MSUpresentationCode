403c38757f09a370d306184adc9e1fd6812df99e
Daina Bouquin <dbouquin@gmail.com>
2014-12-02 21:40:04 -0500

    fix typos in comments

--- CosineSimExample.py
+++ CosineSimExample.py
@@ -7,7 +7,7 @@
 
 #### Define set of example documents:
 
-# In[1]:
+# In[2]:
 
 documents = (
 "A man lives in Montana",
@@ -19,7 +19,7 @@ documents = (
 
 #### Instantiate the Sklearn TF-IDF Vectorizer and transform our documents into the TF-IDF matrix:
 
-# In[2]:
+# In[4]:
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 tfidf_vectorizer = TfidfVectorizer()
@@ -27,7 +27,7 @@ tfidf_matrix = tfidf_vectorizer.fit_transform(documents)
 print tfidf_matrix.shape
 
 
-#### We now have the TF-IDF matrix (tfidf_matrix) for each document (the number of rows of the matrix) with 11 tf-idf terms (the number of columns from the matrix), we can calculate the Cosine Similarity between the first document (“The sky is blue”) with each of the other documents of the set:
+#### We now have the TF-IDF matrix (tfidf_matrix) for each document (the number of rows of the matrix) with 15 tf-idf terms (the number of columns from the matrix), we can calculate the Cosine Similarity between the first document (“A man lives in Montana”) with each of the other documents of the set:
 
 # In[3]:
 
@@ -35,7 +35,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 cosine_similarity(tfidf_matrix[0:1], tfidf_matrix)
 
 
-#### The tfidf_matrix[0:1] is the Scipy operation to get the first row of the sparse matrix and the resulting array is the Cosine Similarity between the first document with all documents in the set. Note that the first value of the array is 1.0 because it is the Cosine Similarity between the first document with itself. Also note that due to the presence of similar words on the third document (“The sun in the sky is bright”), it achieved a better score.
+#### The tfidf_matrix[0:1] is the Scipy operation to get the first row of the sparse matrix and the resulting array is the Cosine Similarity between the first document with all documents in the set. Note that the first value of the array is 1.0 because it is the Cosine Similarity between the first document with itself. Also note that due to the presence of similar words on the second document (“Interesting people live in Montana”), it achieved a better score.
 
 #### Continue: Check the angle between the first and third documents:
 
